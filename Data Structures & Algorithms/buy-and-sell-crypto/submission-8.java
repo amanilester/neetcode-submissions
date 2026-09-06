6db5c5f4034884156f3ec7be5016bdba8bb4e89c
Amani Lester <118773252+amanilester@users.noreply.github.com>
2026-09-06 16:10:26 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = 0;
+        for(int sell = 1; sell < prices.length; sell++) {
+            if(prices[buy] > prices[sell])
+                buy = sell;
+            else
+                profit = Math.max(prices[sell] - prices[buy], profit);
+        }
+        return profit;
+    }
+}
